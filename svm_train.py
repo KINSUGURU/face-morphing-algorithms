7bb7807394403c81c61a5fdc9084d633a6896e39
mataktelis11 <mataktelis01@gmail.com>
2023-09-25 19:00:19 +0300

    Added plots

--- svm_train.py
+++ svm_train.py
@@ -2,8 +2,11 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import svm
 from sklearn import metrics
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import argparse
 import joblib
+import matplotlib.pyplot as plt
+import numpy as np
 
 
 def parse_arguments():
@@ -15,6 +18,8 @@ def parse_arguments():
                     'Class labels: 0 -> bona fide, 1 -> morphed',
                     epilog='')
 
+
+    parser.add_argument('-v', '--visualize', action='store_true', help='Plot graphs for the dataset and Confusion matrix')  # on/off flag
     
     requiredArgs = parser.add_argument_group('Required arguments')
 
@@ -63,10 +68,10 @@ def main():
 
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=0.2, random_state=23)
 
-    # print("x train")
-    # print(X_train)
-    # print("y train")
-    # print(y_train)
+    print("x train")
+    print(X_train)
+    print("y train")
+    print(y_train)
 
 
     # Creating Support Vector Machine Model
@@ -82,9 +87,70 @@ def main():
     joblib.dump(clf, args.output)
     print("Model saved!")
 
-    print("Performing test...")
+    print("Performing tests...")
+
     y_predict = clf.predict(X_test)
     print("Accuracy (with testing set):",metrics.accuracy_score(y_test, y_predict))
+
+    # confusion matrix
+    cm_test = confusion_matrix(y_test, y_predict)#labels=['bona fide', 'morphed']
+    print("Confusion Matrix on Testing Set")
+    print(cm_test)
+
+
+    y_predict = clf.predict(X_train)
+    print("Accuracy (with training set):",metrics.accuracy_score(y_train, y_predict))
+
+    # confusion matrix
+    cm_train = confusion_matrix(y_train, y_predict)#labels=['bona fide', 'morphed']
+    print("Confusion Matrix on Training Set")
+    print(cm_train)
+
+
+
+    if args.visualize:
+
+
+        # Pie charts
+        # https://www.w3schools.com/python/matplotlib_pie_charts.asp
+        
+        num_train_0 = len(y_train[y_train[:]==0])
+        num_train_1 = len(y_train[y_train[:]==1])
+        pie_train = np.array([num_train_0, num_train_1])
+        labels_train = [f"bona fide: {num_train_0}", f"morphed: {num_train_1}"]
+
+        num_test_0 = len(y_test[y_test[:]==0])
+        num_test_1 = len(y_test[y_test[:]==1])
+        pie_test = np.array([num_test_0, num_test_1])
+        labels_test = [f"bona fide: {num_test_0}", f"morphed: {num_test_1}"]
+
+
+        # https://matplotlib.org/3.1.1/gallery/subplots_axes_and_figures/figure_title.html
+        fig, axs = plt.subplots(1, 2, constrained_layout=True)
+
+
+        fig.suptitle('Dataset Overview', fontsize=16)
+
+        axs[0].pie(pie_train, labels=labels_train, shadow=True, explode=[0.1, 0])
+        axs[0].set_title('Training set')
+        axs[0].legend(title = "Training set:")
+
+        axs[1].pie(pie_test, labels=labels_test, shadow=True, explode=[0.1, 0])
+        axs[1].set_title('Testing Set')
+        axs[1].legend(title = "Testing set:")
+
+
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm_test)
+        disp.plot()
+        plt.title("Confusion Matrix on Testing Set")
+
+        disp = ConfusionMatrixDisplay(confusion_matrix=cm_train)
+        disp.plot()
+        plt.title("Confusion Matrix on Training Set")
+
+
+        plt.show()
+
 
 
 
